cc5ac6a803858d24163c98ddbe2337fb69102597
Ariel Augusto Gondim Guedes <81493023+arielgds@users.noreply.github.com>
2024-11-03 13:20:26 -0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,35 +1,35 @@
-// LEMBRAR DE USAR OS COMANDOS PARA ENTRAR NA PASTA DE BACKEND E  O NPM START
 import ui from "./ui.js"
 import api from "./api.js"
 
 document.addEventListener("DOMContentLoaded", () => {
-    ui.renderizarPensamentos()
+  ui.renderizarPensamentos()
 
-    const formularioPensamento = document.getElementById("pensamento-form")
-    formularioPensamento.addEventListener("submit",manipularSubmissaoFormulario)
-    
-    const botaoCancelar = document.getElementById("botao-cancelar")
-    botaoCancelar.addEventListener("click", () => {
-        formularioPensamento.reset()
-    })
+  const formularioPensamento = document.getElementById("pensamento-form")
+  const botaoCancelar = document.getElementById("botao-cancelar")
+
+  formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+  botaoCancelar.addEventListener("click", manipularCancelamento)
 })
 
-async function manipularSubmissaoFormulario(event){
-    event.preventDefault()
-    const id = document.getElementById("pensamento-id").value
-    const conteudo = document.getElementById("pensamento-conteudo").value
-    const autoria = document.getElementById("pensamento-autoria").value
+async function manipularSubmissaoFormulario(event) {
+  event.preventDefault()
+  const id = document.getElementById("pensamento-id").value
+  const conteudo = document.getElementById("pensamento-conteudo").value
+  const autoria = document.getElementById("pensamento-autoria").value
 
-    try{
-        if(id) {
-            await api.editarPensamento( {id, conteudo, autoria})
-        } else {
-            await api.salvarPensamento({conteudo, autoria})
-        }
-        ui.renderizarPensamentos()
+  try { 
+    if(id) {
+      await api.editarPensamento({ id, conteudo, autoria })
+    } else {
+      await api.salvarPensamento({ conteudo, autoria })
     }
-    catch{
-        alert('Erro ao salvar pensamento')
-    }
+    ui.renderizarPensamentos()
+  }
+  catch {
+    alert("Erro ao salvar pensamento")
+  }
+}
 
+function manipularCancelamento() {
+  ui.limparFormulario()
 }
--- js/ui.js
+++ js/ui.js
@@ -1,67 +1,68 @@
-// LEMBRAR DE USAR OS COMANDOS PARA ENTRAR NA PASTA DE BACKEND E  O NPM START
 import api from "./api.js"
 
 const ui = {
 
-    preencherFormulario(pensamentoId){
-        const pensamento = api.buscarPensamentoPorId(pensamentoId)
-        document.getElementById('pensamento-id').value = pensamento.id
-        document.getElementById('pensamento-conteudo').value = pensamento.conteudo
-        document.getElementById('pensamento-autoria').value = pensamento.autoria
-    },
+  async preencherFormulario(pensamentoId) {
+    const pensamento = await api.buscarPensamentoPorId(pensamentoId)
+    document.getElementById("pensamento-id").value = pensamento.id
+    document.getElementById("pensamento-conteudo").value = pensamento.conteudo
+    document.getElementById("pensamento-autoria").value = pensamento.autoria
+  },
 
-    async renderizarPensamentos() {
+  limparFormulario() {
+    document.getElementById("pensamento-form").reset();
+  },
+  
+  async renderizarPensamentos() {
     const listaPensamentos = document.getElementById("lista-pensamentos")
 
     try {
-        const pensamentos = await api.buscarPensamentos()
-        pensamentos.forEach(ui.adicionarPensamentoNaLista)
+      const pensamentos = await api.buscarPensamentos()
+      pensamentos.forEach(ui.adicionarPensamentoNaLista)
     }
     catch {
-        alert('Erro ao renderizar pensamentos')
+      alert('Erro ao renderizar pensamentos')
     }
-    },
+  },
 
-    adicionarPensamentoNaLista(pensamento){
-        const listaPensamentos = document.getElementById('lista-pensamentos');
-        const li = document.createElement("li");
-        li.setAttribute('data-id', pensamento.id)
-        li.classList.add('li-pensamento')
+  adicionarPensamentoNaLista(pensamento) {
+    const listaPensamentos = document.getElementById("lista-pensamentos")
+    const li = document.createElement("li")
+    li.setAttribute("data-id", pensamento.id)
+    li.classList.add("li-pensamento")
 
-        const iconeAspas = document.createElement('img')
-        iconeAspas.src = 'assets/imagens/aspas-azuis.png'
-        iconeAspas.alt = 'Aspas azuis'
-        iconeAspas.classList.add('icone-aspas')
-        
-        const pensamentoConteudo = document.createElement('div')
-        pensamentoConteudo.textContent = pensamento.conteudo
-        pensamentoConteudo.classList.add('pensamento-conteudo')
-        
-        const pensamentoAutoria = document.createElement('div')
-        pensamentoAutoria.textContent = pensamento.autoria
-        pensamentoAutoria.classList.add('pensamento-autoria')
+    const iconeAspas = document.createElement("img")
+    iconeAspas.src = "assets/imagens/aspas-azuis.png"
+    iconeAspas.alt = "Aspas azuis"
+    iconeAspas.classList.add("icone-aspas")
 
-        //criando o botão editar
-        const botaoEditar = document.createElement('button')
-        botaoEditar.classList.add('botao-editar')
-        botaoEditar.onclick = () => ui.preencherFormulario(pensamento.id)
-        
-        const iconeEditar = document.createElement("img")
-        iconeEditar.src = "assets/imagens/icone-editar.png"
-        iconeEditar.alt = "Editar"
-        botaoEditar.appendChild(iconeEditar)
+    const pensamentoConteudo = document.createElement("div")
+    pensamentoConteudo.textContent = pensamento.conteudo
+    pensamentoConteudo.classList.add("pensamento-conteudo")
 
-        const icones = document.createElement('div')
-        icones.classList.add('icones')
-        icones.appendChild(botaoEditar)
+    const pensamentoAutoria = document.createElement("div")
+    pensamentoAutoria.textContent = pensamento.autoria
+    pensamentoAutoria.classList.add("pensamento-autoria")
 
-        //mandando tudo para a tela
-        li.appendChild(iconeAspas)
-        li.appendChild(pensamentoConteudo)
-        li.appendChild(pensamentoAutoria)
-        li.appendChild(icones)
-        listaPensamentos.appendChild(li)
-    }
+    const botaoEditar = document.createElement("button")
+    botaoEditar.classList.add("botao-editar")
+    botaoEditar.onclick = () => ui.preencherFormulario(pensamento.id)
+
+    const iconeEditar = document.createElement("img")
+    iconeEditar.src = "assets/imagens/icone-editar.png"
+    iconeEditar.alt = "Editar"
+    botaoEditar.appendChild(iconeEditar)
+
+    const icones = document.createElement("div")
+    icones.classList.add("icones")
+    icones.appendChild(botaoEditar)
+
+    li.appendChild(iconeAspas)
+    li.appendChild(pensamentoConteudo)
+    li.appendChild(pensamentoAutoria)
+    li.appendChild(icones)
+    listaPensamentos.appendChild(li)
+  }
 }
 
 export default ui
